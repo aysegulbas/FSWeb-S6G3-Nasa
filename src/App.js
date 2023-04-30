aed9f9af938b39e13867be72c7b831970e6aa199
Aysegul BAS <aysegul.bas@arkas.com>
2023-04-30 17:41:15 +0300

    nasa-II görevi

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ import Apodcomponent from "./component/Apodcomponent";
 
 function App() {
   const [apod,setApod]=useState();
-  const [date,setDate]=useState(new Date().toISOString().split("T")[0]);
+  const [datePicker,setDatePicker]=useState(new Date().toISOString().split("T")[0]);
   
   useEffect(()=>{
   axios
-  .get("https://api.nasa.gov/planetary/apod?api_key=Pl0f3w8rqfP0ha9hl50PBRg1RuiVi5hcOso0Q9x2&date=" + date)
+  .get("https://api.nasa.gov/planetary/apod?api_key=Pl0f3w8rqfP0ha9hl50PBRg1RuiVi5hcOso0Q9x2&date=" + datePicker)
   .then((res)=>{
   console.log("nasa",res.data)
   setApod(res.data);
   
   })
 
-},[date])
+},[datePicker])
 
   return (
     <div className="App">
-      <input type="date"
-            value={date}
-            onChange={(e)=>setDate(e.target.value)}/>
-      <Apodcomponent apod={apod}/>
+      
+      <Apodcomponent apod={apod}
+      dateChange={setDatePicker}
+      currentDate={datePicker}/>
       
     </div>
   );
--- src/component/Apodcomponent.js
+++ src/component/Apodcomponent.js
@@ -1,17 +1,38 @@
 import React from "react";
-const Apodcomponent=({apod})=>{
-    return (
+const Apodcomponent=({apod,dateChange,currentDate})=>{
+  
+  return (
     <div>{apod?(
         <div>
-           
-          <img src={apod.url}/>
-          <h1>{apod.title}</h1>
-          <p>{apod.explanation}</p>
+          <header className="header">Evreni Keşfedelim</header>
+        <div className="container">
+                  <div className="background"style={{ backgroundImage: `url(${apod.hdurl})` }}></div>
+
+          <div className="cerceve">
+            <div className="tarih">
+                <input type="date"
+                value={currentDate}
+                onChange={(e)=>dateChange(e.target.value)}/>
+            </div>  
+            <div className="govde">
+              <img src={apod.url} width="700" height="500" />
+                <div className="icerik-yazi">
+                  <h1>{apod.title}</h1>
+                  <p>{apod.explanation}</p>
+                </div>
+            </div>
+          </div>
+
+        </div>
+
         </div>)
+
+
+
           :
           (<p>...yükleniyor</p>)
-          }
-      </div>)
+          
+      }</div>)
 }
 
 export default Apodcomponent;
